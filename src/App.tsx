53fab9f6b3584600793654fcf64f9a7215d848e2
renangrothe <renansuana@gmail.com>
2025-12-02 21:11:44 -0300

    branch masater sem rascunhos

--- src/App.tsx
+++ src/App.tsx
@@ -23,18 +23,10 @@ function App() {
 		<Location />
 	    </ScrollAnimation>
 
-	    <section id="moradores" className="h-[50vh] flex items-center justify-center border-t border-diretoria-yellow/10">
-		    <h2 className="text-4xl text-gray-500">Próxima Seção: Moradores</h2>
-	    </section>
-
 
 	    <ScrollAnimation>
 		<Testimonials />
 	    </ScrollAnimation>
-
-	    <section id="faq" className="h-[50vh] flex items-center justify-center border-t border-diretoria-yellow/10">
-		    <h2 className="text-4xl text-gray-500">Próxima Seção: FAQ</h2>
-	    </section>
 
       </main>
     </div>
